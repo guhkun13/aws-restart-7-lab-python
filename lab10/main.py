f810c3fbf6df6ea6922fc8dac85fcbe50997a672
Your Name <you@example.com>
2023-11-06 13:52:30 +0000

    add lab10

--- lab10/main.py
+++ lab10/main.py
@@ -1,28 +1,77 @@
-import jsonFileHandler as jfh
+var_to_be_removed = ["ORIGIN", "1", "61", "//"]
+filename = "preproinsulin-seq.txt"
 
-data = jfh.readJsonFile('files/insulin.json')
+new_sequence = []
+with open(filename, 'r') as f:
+    lines = f.readlines()
+    print("input string: ", lines)
+    for line in lines:
+        remove_newline = line.replace("\n", " ")
+        split_line = remove_newline.split(" ")
+        
+        for c in split_line:
+            if not c in var_to_be_removed and c != "":
+                new_sequence.append(c)
 
-if data != "" :
-    bInsulin = data['molecules']['bInsulin']
-    aInsulin = data['molecules']['aInsulin']
-    insulin = bInsulin + aInsulin
-    molecularWeightInsulinActual = data['molecularWeightInsulinActual']
-    
-    print('bInsulin: ' + bInsulin)
-    print('aInsulin: ' + aInsulin)
-    print('molecularWeightInsulinActual: ' + str(molecularWeightInsulinActual))
-    
-    # Calculating the molecular weight of insulin  
-    # Getting a list of the amino acid (AA) weights  
-    aaWeights = data['weights']
-    # Count the number of each amino acids  
-    aaCountInsulin = ({x: float(insulin.upper().count(x)) for x in ['A','C','D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'P', 'Q', 'R','S', 'T','V', 'W', 'Y']})  
-    # Multiply the count by the weights  
-    molecularWeightInsulin = sum({x: (aaCountInsulin[x]*aaWeights[x]) for x in
-    ['A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'P', 'Q', 'R','S', 'T', 'V', 'W', 'Y']}.values())  
-    print("The rough molecular weight of insulin: " +
-    str(molecularWeightInsulin))
-    print("Percent error: " + str(((molecularWeightInsulin - molecularWeightInsulinActual)/molecularWeightInsulinActual)*100))
+new_sequence = "".join(new_sequence)
+print(f'\nnew sequence is = {new_sequence} \nlength = {len(new_sequence)}')
 
+file_seq_clean='preproinsulin-seq-clean.txt'
+with open(file_seq_clean, 'w') as f:
+    f.write(new_sequence)
+
+file_isinsulin_seq_clean = 'Isinsulin-seq-clean.txt'
+file_binsulin_seq_clean = 'binsulin-seq-clean.txt'
+file_cinsulin_seq_clean = 'cinsulin-seq-clean.txt'
+file_ainsulin_seq_clean = 'ainsulin-seq-clean.txt'
+
+# dictionary of file and bit to save
+
+dict_filename_and_sequence = [
+        {
+            'filename': file_isinsulin_seq_clean,
+            'seq_start': 1,
+            'seq_end': 24,
+        },
+        {
+            'filename': file_binsulin_seq_clean,
+            'seq_start': 25,
+            'seq_end': 54,
+        },
+        {
+            'filename': file_cinsulin_seq_clean,
+            'seq_start': 55,
+            'seq_end': 89,
+        },
+        {
+            'filename': file_ainsulin_seq_clean,
+            'seq_start': 90,
+            'seq_end': 110,
+        }
+    ]
+
+
+print("\n=> process to write some sequence to files")
+for data in dict_filename_and_sequence:
+    fname = data['filename']
+    ss = data['seq_start']
+    es = data['seq_end']
+    with open('result/'+fname, 'w') as f:
+        new_line = new_sequence[ss-1:es]
+        print(f'get idx {ss}-{es} = [{new_line}] write to [{fname}]')
+        f.write(new_line)
+        
+        
+print("--- writing done ---")
+# validate the result is as expected
+validate_new_seq = ""
+for data in dict_filename_and_sequence:
+    with open('result/'+data['filename'], 'r') as f:
+        line = f.readline()
+        validate_new_seq += line
+        
+print("is valid? => ", end = "")
+if new_sequence == validate_new_seq:
+    print("VALID")
 else:
-    print("Error. Exiting program")
+    print("INVALID")
