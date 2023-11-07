a73ecdc1a4b3342cb4ed8aec0628b77d6e764c00
Your Name <you@example.com>
2023-11-07 13:54:40 +0000

    update lab-10-bareng

--- lab10/lab-10-bareng.py
+++ lab10/lab-10-bareng.py
@@ -1,12 +1,27 @@
 from main import get_my_data
 
 
+
 # open file
 with open('lab-10-bareng/preproinsulin-seq-clean.txt', 'r') as f:
     lines = f.readlines()
     # membaca baris per baris
-    for line in lines:
-        print(line)
+    line = lines[0]
+    print(line)
     
-    mydata = get_my_data()
+    # 1-24 = 0-24
+    pertama = line[0:24]
+    print(pertama, end="")
+    # 25 - 54 
+    kedua = line[24:54]
+    print(kedua, end="")
+    # 55-89
+    ketiga = line[54:89]
+    print(ketiga, end="")
+    # 90 - 110
+    keempat = line[89:110]
+    print(keempat, end="")
+    
         
+
+print("\n",len(pertama), len(kedua), len(ketiga), len(keempat))
